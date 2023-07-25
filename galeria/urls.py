04148f3f587c2a2c3247745ebab89a844e2d6d43
Lucas Alvws <sociallucasalves@gmail.com>
2023-07-25 13:35:51 -0300

    Aula 5 - colocamos os html e ajustamos os static dirs

--- galeria/urls.py
+++ galeria/urls.py
@@ -1,7 +1,8 @@
 
 from django.urls import path, include
-from .views import index
+from .views import index, imagem
 
 urlpatterns = [
-    path('', index)
+    path('', index, name='index'),
+    path('imagem/',imagem, name='imagem')
 ]
