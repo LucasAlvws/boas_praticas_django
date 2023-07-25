67e3d830e00b7efc4e504468a48fe345cbc9b085
Lucas Alvws <sociallucasalves@gmail.com>
2023-07-25 15:46:01 -0300

    Aula 1 Banco de dados - criando models

--- galeria/views.py
+++ galeria/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Fotografia
+
+
 
 
 def index(request):
-    
-    return render(request, 'galeria/index.html')
+    querry_set = Fotografia.objects.all()
+    return render(request, 'galeria/index.html', {'cards' : querry_set})
 
 
 def imagem(request):
