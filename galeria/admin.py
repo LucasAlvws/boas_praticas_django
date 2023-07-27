abef26c82280abddcf370e27f371cef6b957f9e9
Lucas Alvws <sociallucasalves@gmail.com>
2023-07-26 22:07:26 -0300

    Admin feito

--- galeria/admin.py
+++ galeria/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Fotografia
 
 # Register your models here.
+
+class FotografiaList(admin.ModelAdmin):
+    list_display = ("id", "nome", "legenda", "categoria", "foto", "publicada")
+    list_display_links = ("id", "nome")
+    search_fields = ("nome",)
+    list_filter = ("categoria",)
+    list_editable = ("publicada",)
+    list_per_page = 10
+
+admin.site.register(Fotografia, FotografiaList)
--- galeria/views.py
+++ galeria/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Fotografia
 
@@ -6,9 +6,10 @@ from .models import Fotografia
 
 
 def index(request):
-    querry_set = Fotografia.objects.all()
+    querry_set = Fotografia.objects.order_by("-data_foto").filter(publicada=True)
     return render(request, 'galeria/index.html', {'cards' : querry_set})
 
 
-def imagem(request):
-    return render(request, 'galeria/imagem.html')
+def imagem(request, id):
+    foto = get_object_or_404(Fotografia, pk=id, publicada=True)
+    return render(request, 'galeria/imagem.html', {'fotografia':foto})
