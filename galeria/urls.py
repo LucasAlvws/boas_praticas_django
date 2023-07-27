df41279fe1f08c0cb2955899c3b50fd56d5a4097
Lucas Alvws <sociallucasalves@gmail.com>
2023-07-26 22:58:26 -0300

    buscar adicionado

--- galeria/urls.py
+++ galeria/urls.py
@@ -1,8 +1,9 @@
 
 from django.urls import path, include
-from .views import index, imagem
+from .views import index, imagem, buscar
 
 urlpatterns = [
     path('', index, name='index'),
-    path('imagem/<int:id>',imagem, name='imagem')
+    path('imagem/<int:id>',imagem, name='imagem'),
+    path('buscar', buscar, name='buscar'),
 ]
--- galeria/views.py
+++ galeria/views.py
@@ -13,3 +13,12 @@ def index(request):
 def imagem(request, id):
     foto = get_object_or_404(Fotografia, pk=id, publicada=True)
     return render(request, 'galeria/imagem.html', {'fotografia':foto})
+
+def buscar(request):
+    querry_set = Fotografia.objects.order_by("-data_foto").filter(publicada=True)
+
+    if "buscar" in request.GET:
+        nome_buscar = request.GET['buscar']
+        if nome_buscar:
+            querry_set = querry_set.filter(nome__icontains = nome_buscar)
+    return render(request, 'galeria/buscar.html', {'cards' : querry_set})
